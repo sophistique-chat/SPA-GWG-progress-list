5e25b5ac6d7f58533bc11df2f75fcd0407729fc7
sachin3 <contact.sachin@hotmail.com>
2018-03-28 20:13:38 -0400

    Integrated JS Code - Initial Checkin

--- study-buddies-spa.js
+++ study-buddies-spa.js
@@ -1,4 +1,208 @@
-$(function(){
+/* 
+  * Copyright 2018 Study Buddies
+  *  Notes [Sachin-3/22/2018]
+  *  @Node class is used to represent checklist node
+  *  Code is divided into different namespaces.
+  *  SBP.Enums - stores fixed enums.
+  *  SBP.Consts - stores constants.
+  *  SBP.Data - stores common data required for page.
+  *  SBP.UI - code for top level UI related calls. <try-catch layer>
+  *  SBP.UIHelper - helper code for UI layer. Only called from SBP.UI
+  *  SBP.Events - code to bind events and handle callbacks. <try-catch layer>
+  *  SBP.EventsHelper - helper code for Event layer.  Only called from SBP.Events
+  *  SBP.Helper - Generic helper code which do not belong to UI and Events layer
+  *  localStorage api's are used for client side storage.
+ */
+
+SBP = {};
+
+let Node = function (parent, isChecked) {
+    let privateMembers = {
+        parent: parent,
+        children: [],
+        isChecked: isChecked,
+        group: SBP.Enums.Groups.NONE,
+        status: SBP.Enums.Status.NOTCOMPLETED
+    }
+
+    return {
+        get: function (property) {
+            if (privateMembers.hasOwnProperty(property)) {
+                return privateMembers[property];
+            }
+        },
+
+        set: function (property, value) {
+            if (privateMembers.hasOwnProperty(property)) {
+                privateMembers[property] = value;
+            }
+        }
+    }
+};
+
+SBP.Enums = {
+    Groups: Object.freeze({
+        NONE: 'none',
+        HTML: 'html',
+        CSS: 'css',
+        JAVASCRIPT: 'javascript',
+        JQUERY: 'jquery',
+        PROJECTS: 'projects'
+    }),
+    Status: Object.freeze({
+        NOTCOMPLETED: 0,
+        PENDING: 1,
+        COMPLETED: 2
+    })
+
+};
+
+SBP.Consts = {
+    DATA_ATTR_CATEGORY: 'data-category-type',
+    ATTR_CATEGORY: 'category-type',
+    LAST_PAGE_STATE: 'study-buddies-project-progress'
+
+}
+
+SBP.Data = {
+    checkListMap: new Object()
+}
+
+SBP.UI = {
+    bindPage: function () {
+        try {
+            SBP.UIHelper.loadData();
+            $('#main-container').find('li input').each(function () {
+                let id = $(this).attr("id");
+                if (SBP.Data.checkListMap[id]) {
+                    let isChecked = SBP.Data.checkListMap[id].isChecked;
+                    $(this).prop('checked', isChecked);
+                }
+            });
+        }
+        catch (ex) {
+            console.log(ex.message);
+        }
+    },
+
+    refreshPage: function () {
+        try {
+            // Update progress bars and other UI changes.
+        }
+        catch (ex) {
+            console.log(ex.message);
+        }
+    },
+
+    savePage: function () {
+        try {
+            localStorage[SBP.Consts.LAST_PAGE_STATE] = JSON.stringify(SBP.Data.checkListMap);
+        }
+        catch (ex) {
+            console.log(ex.message);
+        }
+    },
+
+    showError: function (error) {
+        if (error.message) {
+            console.log(ex.message);
+        } else {
+            console.log(error);
+        }
+    }
+};
+
+SBP.UIHelper = {
+    loadData: function () {
+        let data = localStorage[SBP.Consts.LAST_PAGE_STATE];
+        if (data) {
+            let checkListMap = JSON.parse(data);
+            if (checkListMap) {
+                SBP.Data.checkListMap = checkListMap;
+            }
+        }
+    },
+
+    fillNode: function (target, node) {
+        let category = target.attr(SBP.Consts.DATA_ATTR_CATEGORY);
+        node.group = category;
+        let isChecked = target.is(':checked');
+        node.isChecked = isChecked;
+    }
+};
+
+SBP.Events = {
+    bindEvents: function () {
+        try {
+            $('#main-container').on("click", "li input", function (e) {
+                SBP.Events.onListItemClicked($(e.target));
+            });
+        }
+        catch (ex) {
+            SBP.UI.showError(ex);
+        }
+    },
+
+    onListItemClicked: function (target) {
+        try {
+            // Create node and store in map
+            let id = target.attr("id");
+            SBP.Helpers.updateCheckListMap(id, target);
+            var parent = target.parent();
+            if (parent.hasClass('lessonTitle')) {
+                parent.find('li input').each(function () {
+                    let childId = $(this).attr("id");
+                    SBP.Helpers.updateCheckListMap(childId, $(this));
+                });
+            }
+
+            // Refresh Page
+            SBP.UI.refreshPage();
+            // Save Page
+            SBP.UI.savePage();
+        }
+        catch (ex) {
+            console.log(ex.message);
+        }
+    }
+};
+
+SBP.EventsHelper = {
+
+};
+
+SBP.Helpers = {
+    updateCheckListMap: function (id, target) {
+        let node = null;
+        if (!SBP.Data.checkListMap[id]) {
+            node = new Node(null, false);
+            if (node) {
+                SBP.Data.checkListMap[id] = node;
+            }
+        } else {
+            node = SBP.Data.checkListMap[id];
+        }
+        SBP.UIHelper.fillNode(target, node);
+        return node;
+    }
+};
+
+$(function () {
+    try {
+        SBP.UI.bindPage();
+        SBP.Events.bindEvents();
+    }
+    catch (ex) {
+        console.log(ex.message);
+    }
+});
+
+
+
+
+
+/*Angel & Steve*/
+$(function () {
   $(".state-buttons").html($(".state-buttons").attr("data-start-state-text"));
   $(".tabs-nav li:first-child a").click();
 });
